288d11c935eedf3a07f2e2854f21da2188c0d8de
Aluno <Aluno@SNCTM202LABF08>
2024-03-11 20:58:16 -0300

    Adicionar arquivos de projeto.

---
+++ ControleEPIApp/ControleEPIApp/Models/Funcionario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleEPIApp.Models
+{
+    public class Funcionario
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string matricula { get; set;}
+        public string epi { get; set; }
+        public DateTime data_entrega  { get; set; }
+        public DateTime data_vencimento { get; set; }
+    }
+}
